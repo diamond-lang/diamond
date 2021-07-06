87bbc4a0e8f0299471d82cf472757fc61aa83fd0
Alonso Zamorano <alonso.zamorano97@gmail.com>
2021-07-06 13:01:05 -0400

    Changed tests to test functionality, not the implementation (Fixes #8)

---
+++ test/test.py
@@ -0,0 +1,45 @@
+import subprocess
+from os.path import exists
+
+def print_expected_vs_result(expected, result):
+    expected = expected.split("\n")
+    print("  Expected:")
+    for line in expected:
+        print("    " + line)
+
+    result = result.split("\n")
+    print("  Result:")
+    for line in result:
+        print(str("    " + line))
+
+def test(file, expected):
+    print(f"testing {file}... ", end='')
+    if not exists(file):
+        print("File not found :(")
+        return
+    result = subprocess.run(['../builddir/diamond', 'run', file], stdout=subprocess.PIPE, text=True)
+    if result.stdout == expected:
+        print('OK')
+    else:
+        print('Failed :(')
+        print_expected_vs_result(expected, result.stdout)
+        quit()
+
+test("floats.dm", "4.3\n" + \
+                  "6\n" + \
+                  "0.8\n" + \
+                  "0.33333\n")
+
+test("arithmetic_expressions.dm", "16\n")
+
+test("comparison_expressions.dm", "false\n" + \
+                                  "true\n" + \
+                                  "true\n" + \
+                                  "true\n" + \
+                                  "false\n")
+
+test("assignments.dm", "113\n" + \
+                       "true\n")
+
+test("booleans.dm", "true\n" + \
+                    "true\n")
