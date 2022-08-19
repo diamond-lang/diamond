101d4887d0182ea4432a1e4c21d04c5df5654eb8
Alonso Zamorano <alonso.zamorano97@gmail.com>
2022-08-19 17:43:06 -0400

    Added tests for structs

--- test.py
+++ test.py
@@ -4,17 +4,6 @@ import re
 import platform
 import subprocess
 
-def print_expected_vs_result(expected, result):
-    expected = expected.split("\n")
-    print("  Expected:")
-    for line in expected:
-        print("    " + line)
-
-    result = result.split("\n")
-    print("  Result:")
-    for line in result:
-        print("    " + line)
-
 def test(file, expected):
     print(f"testing {file}...  ", end='')
     if not os.path.exists(file):
@@ -26,8 +15,7 @@ def test(file, expected):
     if result == expected:
         print('OK')
     else:
-        print('Failed :(')
-        print_expected_vs_result(expected, result)
+        print('Failed')
         quit()
 
 def get_all_files(folder):
